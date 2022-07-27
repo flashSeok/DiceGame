176d4c3da7d910b12521d35ae0ad1cf3ba19f5f7
Henry Kim <flashSeok@gmail.com>
2022-07-27 15:26:29 +0900

    "앱 올리기"

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
+    // 앱의 화면에 들어오면 가장 먼저 들어오는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstImageView.image = diceArray[0]
+        secondImageView.image = diceArray[0]
     }
+    
+    
 
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        // 첫번째 이미지뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+        
+        // 두번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
+        
+    }
+    
+    
 
 }
 
